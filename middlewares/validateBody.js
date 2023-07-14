8bbb33004b8af5259acfd8bbcbdc1a67c946121f
Alexon7 <alenkaalexon@gmail.com>
2023-07-14 23:16:40 +0200

    fix message joi

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -3,15 +3,14 @@ const { RequestError } = require("../helpers/");
 const validateBody = (contactSchema) => {
     const func = (req, res, next) => {
         const { error } = contactSchema.validate(req.body, { abortEarly: false });
-        // const reqParam = contactSchema.validate(req.body);
-        const emptyField = !Object.keys(req.body).length;
+               const emptyField = !Object.keys(req.body).length;
          
         if (emptyField) {
             throw RequestError(400, "missing fields")
         }
         if (error) {
-            const paramError = error.details[0].path[0];
-            throw (RequestError(400, `missing required ${paramError} field`))
+            
+            throw (RequestError(400, error.message))
         }
         next();
     };
